32019a331fadec77d5a445c0a54252fb85e011e7
GUIPASCHOUD <guillaumepaschoud@yahoo.com>
2023-12-05 20:45:46 +0100

    Liste de musiques

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,40 @@
 "use strict";
 
-let CSV = [];
+const INPUT_ARTIST = document.getElementById("inputArtist");
+const H1_LIST_OF_MUSICS = document.getElementById("titleListOfMusics");
+const MUSICS_LIST_TABLE = document.getElementById("musicsListTable");
 
 // Load CSV
 async function readCsv() {
-	const csv = await d3.csv("data/spotify.csv");
+	const csv = await d3.dsv("|", "data/spotify.csv");
 	return csv;
 }
 
 function prepareUi(csv) {
-	console.log(csv);
-	const artists = [ ...new Set(csv.map( el => {
+	const artists = [...new Set(csv.map(el => {
 		return el.artist;
 	}))];
-	
-	const musics = [ ...new Set(csv.map( el => {
+
+	const musics = [...new Set(csv.map(el => {
 		return el.title;
 	}))];
 
 	uiLoadArtists(artists);
 	uiLoadMusics(musics);
+
+	uiListMusics(csv);
+
+	INPUT_ARTIST.addEventListener("input", (event) => {
+		uiLoadMusicsOfArtist(INPUT_ARTIST.value, csv);
+		console.log("Hey");
+	});
+
+	showStatForMusic([]);
 }
 
 async function main() {
 	const csv = await readCsv();
+
 	prepareUi(csv);
 }
 
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function uiLoadArtists(artists) {
 	const datalist = document.getElementById("artistsList");
 	artists.forEach(element => {
@@ -9,10 +11,43 @@ function uiLoadArtists(artists) {
 
 function uiLoadMusics(musics) {
 	const datalist = document.getElementById("musicsList");
-	console.log(musics);
 	musics.forEach(element => {
 		let option = document.createElement('option');
 		option.value = element;
 		datalist.appendChild(option);
 	});
 }
+
+function uiLoadMusicsOfArtist(artist, db) {
+	console.log("I have been called");
+	const musics = db.filter((el) => el.artist == artist || artist === "all");
+	H1_LIST_OF_MUSICS.innerHTML = `List of music by ${artist}`;
+
+	console.log(musics);
+}
+
+function uiListMusics(musics, index, size) {
+	MUSICS_LIST_TABLE.innerHTML = "";
+	console.log("Ui list");
+	console.log(musics);
+	for (const music in musics) {
+		MUSICS_LIST_TABLE.innerHTML += `
+			<td>${musics[music].title}</td>
+			<td>${musics[music].artist}</td>
+			<td>${musics[music].topGenre}</td>
+			<td>${musics[music].year}</td>
+		`;
+	}
+}
+
+// <td>${musics[music].bpm}</td>
+// <td>${musics[music].nrgy}</td>
+// <td>${musics[music].dnce}</td>
+// <td>${musics[music].dB}</td>
+// <td>${musics[music].live}</td>
+// <td>${musics[music].val}</td>
+// <td>${musics[music].dur}</td>
+// <td>${musics[music].acous}</td>
+// <td>${musics[music].spch}</td>
+// <td>${musics[music].pop}</td>
+
